625a91f93f4f35474e14c7f15d244f6951bea371
Remix Workspace <>
2022-08-24 11:12:14 +0530

    video 3 complete

--- token.sol
+++ token.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.7; //default version of solidity selected as in remix ide
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-contract fury is ERC20 {
+contract Link is ERC20 {
      
      constructor() ERC20("chainlink","LINK") {
 
